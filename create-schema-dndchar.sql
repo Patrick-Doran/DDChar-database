f1c237ad3b7934383a2ce9276bb5c3dbf67b99b4
Patrick Doran <wombatpatrick@gmail.com>
2020-11-05 11:34:48 -0800

    INSERT for user and class

--- create-schema-dndchar.sql
+++ create-schema-dndchar.sql
@@ -6,7 +6,7 @@ CREATE TABLE user(
 CREATE TABLE playerCharacter(
     pc_name varchar(20) NOT NULL, --KEY--
     pc_race varchar(15) NOT NULL,
-    pc_class varchar(15) NOT NULL,
+    pc_class decimal(2,0) NOT NULL,
     pc_level decimal(2,0) NOT NULL
 );
 
@@ -17,14 +17,14 @@ CREATE TABLE background(
 );
 
 CREATE TABLE class(
-    cl_name varchar(15) NOT NULL,
-    cl_id decimal(1,0) NOT NULL --KEY--
+    cl_id decimal(2,0) NOT NULL, --KEY--
+    cl_name varchar(15) NOT NULL
 );
 
 CREATE TABLE classWeapon(
     clwpn_prof char(1) NOT NULL,
     clwpn_name varchar(25) NOT NULL, --KEY FROM JOIN--
-    clwpn_id decimal(1,0) NOT NULL --KEY FROM JOIN--
+    clwpn_id decimal(2,0) NOT NULL --KEY FROM JOIN--
 );
 
 CREATE TABLE weapon(
@@ -35,7 +35,7 @@ CREATE TABLE weapon(
 
 CREATE TABLE classSpell(
     clsp_ability char(3) NOT NULL,
-    clsp_id decimal(1,0) NOT NULL, --KEY FROM JOIN--
+    clsp_id decimal(2,0) NOT NULL, --KEY FROM JOIN--
     clsp_name varchar(25) NOT NULL --KEY FROM JOIN--
 );
 
@@ -47,3 +47,27 @@ CREATE TABLE spell(
     sp_description varchar(144) NOT NULL
 );
 
+--POPULATE TABLE--
+INSERT INTO user(u_key, u_name) 
+VALUES (rowid, 'Matias'),
+    (rowid, 'Patrick'),
+    (rowid, 'Jaeha'),
+    (rowid, 'Loic'),
+    (rowid, 'Adam');
+
+INSERT INTO playerCharacter(pc_name, pc_race, pc_class, pc_level)
+VALUES ('Libson', 'HUMAN', 1, 4);
+
+INSERT INTO class(cl_id, cl_name)
+VALUES(0, 'barbarian'),
+    (1, 'bard'),
+    (2, 'cleric'),
+    (3, 'druid'),
+    (4, 'fighter'),
+    (5, 'monk'),
+    (6, 'paladin'),
+    (7, 'ranger'),
+    (8, 'rogue'),
+    (9, 'sorcerer'),
+    (10, 'warlock'),
+    (11, 'wizard');
