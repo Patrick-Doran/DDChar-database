a6c8dcb5857231b612b64b73c64eb1cc2a3be883
jha359 <jha7699@gmail.com>
2020-11-09 23:47:33 -0800

    10 sql queries

--- queries.sql
+++ queries.sql
@@ -10,7 +10,9 @@ WHERE u_key = pc_key;
 --3) List all non-human player characters--
 SELECT pc_name AS name
 FROM playerCharacter
-WHERE pc_race != 'HUMAN';
+INNER JOIN background
+ON bg_name = pc_name
+WHERE bg_race != 'HUMAN';
 
 --4) How many characters does each Player have--
 SELECT u_name AS user, COUNT(pc_key) AS total
@@ -25,7 +27,7 @@ INNER JOIN playerCharacter, user
 ON classSpell.clsp_id = playerCharacter.pc_class
     AND playerCharacter.pc_key = 0;
 
---6) List all characters that have proficeincy in a weapon that deals 1d6 damage--
+--6) List all characters that have proficiency in a weapon that deals 1d6 damage--
 SELECT pc_name AS name
 FROM playerCharacter
 INNER JOIN classWeapon, weapon
@@ -38,7 +40,7 @@ ON playerCharacter.pc_class = classWeapon.clwpn_id
 SELECT DISTINCT pc_name AS name
 FROM playerCharacter
 INNER JOIN background
-ON pc_race = bg_race
+ON pc_name = bg_name
 WHERE bg_age > 20;
 
 --8) List all spells usable by a druid that are a bonus action--
@@ -65,3 +67,85 @@ ON pc_class = clwpn_id
     AND pc_class = clsp_id
     AND clsp_name = 'animal friendship'
     AND pc_key = u_key;
+
+--11) List spells that the class druid can cast but the class ranger cannot
+SELECT sp_name FROM spell
+INNER JOIN classSpell ON classSpell.clsp_name = spell.sp_name
+WHERE clsp_id = 3 AND clsp_name NOT IN (
+    SELECT clsp_name FROM classSpell
+    WHERE clsp_id = 7);
+
+--12) List how many spells each class has per slot level
+SELECT cl_name AS class, IFNULL(sp_slotLevel, 'N/A') AS slotlevel, COUNT(sp_slotLevel) AS num_spells FROM class
+LEFT JOIN (
+    SELECT * FROM classSpell
+    INNER JOIN spell ON spell.sp_name = classSpell.clsp_name) AS table1 ON table1.clsp_id = class.cl_id
+GROUP BY cl_name, sp_slotLevel;
+
+--13) List characters that have a charm type spell and are proficient with a dagger weapon
+SELECT pc_name FROM playerCharacter
+INNER JOIN class ON class.cl_id = playerCharacter.pc_class
+INNER JOIN classWeapon ON classWeapon.clwpn_id = class.cl_id
+INNER JOIN classSpell ON classSpell.clsp_id = class.cl_id
+INNER JOIN spell ON spell.sp_name = classSpell.clsp_name
+WHERE sp_effect = 'charm' AND clwpn_name = 'dagger'
+GROUP BY pc_name
+
+--14) List spells usable by characters that are Half-Elves
+SELECT sp_name AS spell FROM spell
+INNER JOIN classSpell ON classSpell.clsp_name = spell.sp_name
+INNER JOIN class ON class.cl_id = classSpell.clsp_id
+INNER JOIN playerCharacter ON playerCharacter.pc_class = class.cl_id
+INNER JOIN background ON playerCharacter.pc_name = background.bg_name
+WHERE bg_race = 'HALF-ELF' AND sp_slotLevel <= pc_level
+GROUP BY sp_name
+
+--15) List spells that are exclusively used by wizards
+SELECT sp_name AS spell FROM spell
+INNER JOIN classSpell ON classSpell.clsp_name = spell.sp_name
+WHERE clsp_id = 11 AND sp_name NOT IN (
+    SELECT clsp_name FROM classSpell
+    WHERE clsp_id != 11);
+
+--16) List spells that only rangers cannot use
+SELECT sp_name AS spell FROM spell
+INNER JOIN classSpell ON classSpell.clsp_name = spell.sp_name
+WHERE sp_name NOT IN (
+    SELECT clsp_name FROM classSpell
+    WHERE clsp_id = 7)
+GROUP BY sp_name;
+
+--17) List characters and the weapons they are proficient in
+SELECT pc_name AS character, wpn_name AS weapon FROM playerCharacter, weapon
+INNER JOIN classWeapon ON playerCharacter.pc_class = classWeapon.clwpn_id
+WHERE classWeapon.clwpn_name = weapon.wpn_name AND clwpn_prof = 'T';
+
+--18) List classes without bonus action spells at level 1
+SELECT cl_name AS class FROM class
+WHERE cl_name NOT IN (
+    SELECT cl_name FROM class
+    INNER JOIN classSpell ON classSpell.clsp_id = class.cl_id
+    WHERE clsp_name IN (
+        SELECT sp_name FROM spell
+        WHERE sp_castingtype = 'bonus' AND sp_slotLevel = 1)
+    GROUP BY cl_name);
+
+--19) List classes that can deal damage at 100+ range
+SELECT cl_name AS class FROM class
+INNER JOIN classSpell ON classSpell.clsp_id = class.cl_id
+INNER JOIN spell ON spell.sp_name = classSpell.clsp_name
+WHERE sp_name IN (
+SELECT sp_name FROM spell
+WHERE sp_die != 'N/A' AND sp_range >= 100 AND sp_effect = 'damage')
+GROUP BY cl_name
+UNION
+SELECT cl_name AS class FROM class
+INNER JOIN classWeapon ON classWeapon.clwpn_id = class.cl_id
+INNER JOIN weapon ON weapon.wpn_name = classWeapon.clwpn_name
+WHERE clwpn_prof = 'T' AND wpn_range >= 100;
+
+--20) List the alignments of the characters of each user
+SELECT bg_alignment AS alignment, u_name AS user FROM background, user
+INNER JOIN playerCharacter ON playerCharacter.pc_key = user.u_key
+WHERE bg_name = pc_name
+ORDER BY u_name ASC;
