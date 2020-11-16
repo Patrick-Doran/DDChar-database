189e19a9408eb258bbe010e3876c8efdf1ef5458
Patrick Doran <wombatpatrick@gmail.com>
2020-11-16 14:36:47 -0800

    setup up frontend use

--- DDCharDatabase/DDChar/urls.py
+++ DDCharDatabase/DDChar/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #index
     path('', views.index, name='index'),
+    #/DDChar/character/userName
+    path('character/<userName>/', views.getUser, name='character'),
 ]
--- DDCharDatabase/DDChar/views.py
+++ DDCharDatabase/DDChar/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hellow World, you are on DDChar.")
+
+def getUser(request, userName):
+    return HttpResponse("Welcome %s to the database." % userName)
+
+def listUsers(request):
+    user_list = User.objects
+
+def getCharacter(request, charName, charRace, charLevel):
+    response = "Your character stats are as follows: /n Name: %s /n Race: %s /n Level %s"
+    return HttpResponse(response % charName, charRace, charLevel)
