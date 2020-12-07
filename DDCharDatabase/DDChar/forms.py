b64602f4d6278f93510aec4b33e5019b8fa67bf9
Patrick Doran <wombatpatrick@gmail.com>
2020-12-07 14:19:28 -0800

    fixed placeholders

--- DDCharDatabase/DDChar/forms.py
+++ DDCharDatabase/DDChar/forms.py
@@ -3,7 +3,9 @@ from django.forms import ModelForm
 from .models import *
 
 class spellForm(forms.ModelForm):
-
     class Meta:
         model = Spell
         fields = ['sp_name', 'sp_slotlevel', 'sp_range', 'sp_castingtype', 'sp_effect', 'sp_die']
+
+#class charForm(forms.Form):
+
