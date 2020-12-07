76383e9c8fe6a4f66f35e40a177690e5fa937707
Patrick Doran <wombatpatrick@gmail.com>
2020-12-06 20:19:01 -0800

    Fixed POST

--- DDCharDatabase/DDChar/forms.py
+++ DDCharDatabase/DDChar/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.forms import ModelForm
 from .models import *
 
 class spellForm(forms.ModelForm):
-    
+
     class Meta:
         model = Spell
         fields = ['sp_name', 'sp_slotlevel', 'sp_range', 'sp_castingtype', 'sp_effect', 'sp_die']
